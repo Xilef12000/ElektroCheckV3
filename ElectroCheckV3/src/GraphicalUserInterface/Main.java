30f47c6e6939160b91674a938bd5290a3aeb548c
Vishal Bahatyani <216287111+VishalBahatyani@users.noreply.github.com>
2025-06-14 16:09:44 +0200

    geraeteListe_Implementiert

--- ElectroCheckV3/src/GraphicalUserInterface/Main.java
+++ ElectroCheckV3/src/GraphicalUserInterface/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) 
 	{
 		JFrame MainWindow = new JFrame();
-		MainPanel MainPanel = new MainPanel(MainWindow);
+		MainPanel MainPanel = new MainPanel();
 		MainWindow.setSize(900,600);
 		MainWindow.setContentPane(MainPanel);
 		MainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- ElectroCheckV3/src/GraphicalUserInterface/MainPanel.java
+++ ElectroCheckV3/src/GraphicalUserInterface/MainPanel.java
@@ -1,18 +1,42 @@
 package GraphicalUserInterface;
 
 import javax.swing.*;
+import java.util.*;
+import java.util.List;
 import java.awt.*;
 
 public class MainPanel extends JPanel
 {
-	private JFrame mainWindow;
-
-	public MainPanel(JFrame mainWindow) 
+	List<Geraet> geraeteList = new ArrayList<>();
+	
+	public MainPanel() 
 	{
-		this.mainWindow = mainWindow;
-		
-		//Definiere Layout
+		//Layout setzten
 		this.setLayout(new BorderLayout());
+		
+		//Menuebar erstellen
+		JMenuBar menuBar = new JMenuBar(); // Menüleiste)
+		menuBar.setFont(new Font("Arial", Font.PLAIN, 16));
+		
+		//Menuleiste erstellen
+		JMenu benutzerMenu = new JMenu("Benuter"); 
+		benutzerMenu.setBackground(Color.GRAY);
+		benutzerMenu.setForeground(Color.BLACK);
+		benutzerMenu.setFont(new Font("Segoe UI", Font.BOLD, 16));
+	    
+	    //Eintrage der Menueleiste
+	    JMenuItem Pruefer = new JMenuItem("Pruefer");
+	    JMenuItem Verwalter = new JMenuItem("Verwalter");
+	    benutzerMenu.add(Pruefer);
+	    benutzerMenu.add(Verwalter);
+	    
+	    this.add(menuBar, BorderLayout.NORTH);
 	}
-
+	
+	//Get für GeräteListe
+    List<Geraet> getGeraeteListe(){
+    	return this.geraeteList;
+    }
+    
+    
 }
