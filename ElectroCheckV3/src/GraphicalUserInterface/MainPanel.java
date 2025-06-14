957c72a720b5f02fed67e33a98c5d3a7d1e97740
Vishal Bahatyani <216287111+VishalBahatyani@users.noreply.github.com>
2025-06-14 17:07:06 +0200

    MainPanel_V1.0

--- ElectroCheckV3/src/GraphicalUserInterface/MainPanel.java
+++ ElectroCheckV3/src/GraphicalUserInterface/MainPanel.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.util.*;
 import java.util.List;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainPanel extends JPanel
 {
@@ -17,20 +19,45 @@ public class MainPanel extends JPanel
 		//Menuebar erstellen
 		JMenuBar menuBar = new JMenuBar(); // Menüleiste)
 		menuBar.setFont(new Font("Arial", Font.PLAIN, 16));
+		this.add(menuBar, BorderLayout.NORTH);
 		
 		//Menuleiste erstellen
-		JMenu benutzerMenu = new JMenu("Benuter"); 
+		JMenu benutzerMenu = new JMenu("Benutzer"); 
 		benutzerMenu.setBackground(Color.GRAY);
 		benutzerMenu.setForeground(Color.BLACK);
 		benutzerMenu.setFont(new Font("Segoe UI", Font.BOLD, 16));
+		menuBar.add(benutzerMenu);
 	    
 	    //Eintrage der Menueleiste
-	    JMenuItem Pruefer = new JMenuItem("Pruefer");
-	    JMenuItem Verwalter = new JMenuItem("Verwalter");
-	    benutzerMenu.add(Pruefer);
-	    benutzerMenu.add(Verwalter);
+	    JMenuItem pruefer = new JMenuItem("Pruefer");
+	    JMenuItem verwalter = new JMenuItem("Verwalter");
 	    
-	    this.add(menuBar, BorderLayout.NORTH);
+	    //Aktionlistener für die Menueeintraege
+	    pruefer.addActionListener(new ActionListener() {
+	    	@Override
+            public void actionPerformed(ActionEvent e) { 
+	    		PrueferPanel prueferPanel = new PrueferPanel(MainPanel.this);
+	    		MainPanel.this.add(prueferPanel, BorderLayout.CENTER);
+	    		MainPanel.this.revalidate();
+	    	    MainPanel.this.repaint();
+	    	}
+	    }
+	    );
+	    
+	    verwalter.addActionListener(new ActionListener() {
+	    	@Override
+            public void actionPerformed(ActionEvent e) {
+	    		VerwalterPanel verwalterPanel = new VerwalterPanel();
+	    		MainPanel.this.add(verwalterPanel, BorderLayout.CENTER);
+	    		MainPanel.this.revalidate();
+	    	    MainPanel.this.repaint();
+	    	}
+	    }
+	    );
+	    
+	    benutzerMenu.add(pruefer);
+	    benutzerMenu.add(verwalter);
+	    
 	}
 	
 	//Get für GeräteListe
