c537e5768181625d69cc8e4371bcc3a2f909786a
NiWei17 <nickweigold1705@gmail.com>
2025-06-18 11:46:09 +0200

    Code PrueferPanel in VerwalterPanel kopiert

--- ElectroCheckV3/src/GraphicalUserInterface/VerwalterPanel.java
+++ ElectroCheckV3/src/GraphicalUserInterface/VerwalterPanel.java
@@ -1,13 +1,114 @@
 package GraphicalUserInterface;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
 import javax.swing.*;
 
 public class VerwalterPanel extends JPanel
 {
+	private MainPanel mainPanel;
+	private JList<Geraet> geraetJList;
 
-	public VerwalterPanel() 
+	public VerwalterPanel(MainPanel mainPanel) 
 	{
+		this.mainPanel = mainPanel;
 		
+		//Definiere Layout
+		this.setLayout(new BorderLayout());
+				
+		//Menü-Leiste erstellen
+		createScrollPane();
+						
+		//Button-Leiste erstellen
+		createButtonBar();
 	}
+	
+	//Erstellen einer Scrollbaren Anzeige für alle angelegten Geräte
+	private void createScrollPane() 
+	{
+        //Liste der Geräte aus MainPanel holen
+        List<Geraet> geraete = mainPanel.getGeraeteListe();
 
+        // JList erstellen mit DefaultListModel
+        DefaultListModel<Geraet> listModel = new DefaultListModel<>();
+        for (Geraet g : geraete) 
+        {
+            listModel.addElement(g);
+        }
+
+        geraetJList = new JList<>(listModel);
+        geraetJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        geraetJList.setVisibleRowCount(10);
+
+        JScrollPane scrollPane = new JScrollPane(geraetJList);
+        scrollPane.setPreferredSize(new Dimension(300, 200));
+
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
+	
+	//Erstellen der ButtonBar zum Auswählen der gewünschten Aktion
+	private void createButtonBar() 
+	{
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        
+        //Buttons erstellen
+        JButton einsehenButton = new JButton("Geraet einsehen");
+        JButton pruefungDokButton = new JButton("Pruefung dokumentieren");
+        
+        //Buttons in Panel einfügen
+        buttonPanel.add(pruefungDokButton);
+        buttonPanel.add(einsehenButton);
+        
+        //ButtonPanel in HauptPanel einfügen
+        this.add(buttonPanel, BorderLayout.EAST);
+        
+        
+        //Erstellen der ActionListener für die beiden Button
+        ActionListener einsehenAL = new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                Geraet selected = geraetJList.getSelectedValue();
+                if (selected != null) 
+                {
+                   
+                } 
+                else 
+                {
+
+                }
+
+            }
+        };
+
+        einsehenButton.addActionListener(einsehenAL);
+        
+        
+        ActionListener pruefungDokAL = new ActionListener() 
+        {
+            @Override
+            public void actionPerformed(ActionEvent e) 
+            {
+                Geraet selected = geraetJList.getSelectedValue();
+                if (selected != null) 
+                {
+                   
+                } 
+                else 
+                {
+
+                }
+
+            }
+        };
+
+        pruefungDokButton.addActionListener(pruefungDokAL);         
+	}
 }
