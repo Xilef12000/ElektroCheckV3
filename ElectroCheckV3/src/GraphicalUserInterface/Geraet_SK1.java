8839cf08655dc386f8ba4d5c32e089e9f3bbb782
Manuel König <79515423+Xilef12000@users.noreply.github.com>
2025-06-18 15:15:15 +0200

    changed schutzklasse string

--- ElectroCheckV3/src/GraphicalUserInterface/Geraet_SK1.java
+++ ElectroCheckV3/src/GraphicalUserInterface/Geraet_SK1.java
@@ -8,7 +8,7 @@ public class Geraet_SK1 extends Geraet
 	public Geraet_SK1(String name, Boolean pruefungBestanden, int tageBisAbgelaufen) 
 	{
 		// Constuctor der abstact superclass aufrufen
-		super(name, pruefungBestanden, tageBisAbgelaufen, "Schutzklasse I");
+		super(name, pruefungBestanden, tageBisAbgelaufen, "I");
 		
 		// zuweisen von 'grundeErlaubt'
 		// Elemente von 'grundeErlaubt' sind vom Datentyp/Klasse 'Grund'
--- ElectroCheckV3/src/GraphicalUserInterface/Geraet_SK3.java
+++ ElectroCheckV3/src/GraphicalUserInterface/Geraet_SK3.java
@@ -8,7 +8,7 @@ public class Geraet_SK3 extends Geraet
 	// Kommentare siehe SK1
 	public Geraet_SK3(String name, Boolean pruefungBestanden, int tageBisAbgelaufen) 
 	{
-		super(name, pruefungBestanden, tageBisAbgelaufen, "Schutzklasse III");
+		super(name, pruefungBestanden, tageBisAbgelaufen, "III");
 		this.grundeErlaubt = EnumSet.of(Grund.values()[0], Grund.values()[2], Grund.values()[5], Grund.values()[6]);
 	}
 }
